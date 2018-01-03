023b889f06d3379b3c46d25d37ef6961ab2aae2c
Salvador de la Puente González <salva@unoyunodiez.com>
2018-01-03 21:37:20 +0100

    Reading map metadata

--- src/fpg.ts
+++ src/fpg.ts
@@ -1,32 +1,134 @@
 
+const VERSION_OFFSET = 7;
+
+const MAPS_OFFSET = 1352;
+
+type PromiseResolve = (value?: {} | Thenable<{}>) => void;
+
+type PromiseReject = (error?: any) => void;
+
 class FPGFile {
 
-  _buffer: ArrayBuffer
+  ready: Promise<void>;
 
-  _length: Number
+  version: number;
 
-  constructor(buffer: ArrayBuffer) {
-    this._buffer = new Int8Array(buffer);
-    this._assertFpg();
+  private _bytes: Uint8Array;
+
+  private _length: number;
+
+  private _version: number;
+
+  private _maps: Array<any>;
+
+  private _mapRequests: Array<Array<[PromiseResolve, PromiseReject]>>;
+
+  constructor(bytes: Uint8Array) {
+    this._bytes = bytes;
+    this._length = 0;
+    this._maps = [];
+    this._mapRequests = [];
+    this.version = this._bytes[VERSION_OFFSET];
+    this.ready = this._readMeta();
   }
 
-  _assertFpg(): void {
-    ['f', 'p', 'g', 0x1A, 0x0D, 0x0A, 0x00].forEach((code, index) => {
-      const char = typeof code === 'string' ? code.charCodeAt(0) : code;
-      if (this._buffer[index] !== char) {
-        throw new Error('The file is not a FPG file');
-      }
+  map(index) {
+    if (index < 0) {
+      return Promise.reject('Map indices start in 0');
+    }
+    if (this._length > index) {
+      return Promise.resolve(this._maps[index]);
+    }
+    return new Promise((resolve, reject) => {
+      this._mapRequests[index] = this._mapRequests[index] || [];
+      this._mapRequests[index].push([resolve, reject]);
     });
   }
 
   get length() {
-    return this._length;
+    return this.ready.then(() => this._length);
+  }
+
+  _readMeta(): Promise<void> {
+    const file = this; 
+    return new Promise((resolve, reject) => {
+      setTimeout(() => readMap(MAPS_OFFSET));
+
+      function readMap(offset) {
+        if (offset >= file._bytes.byteLength) {
+          file._confirmAllMaps();
+          resolve();
+          return;
+        }
+
+        const entry: any = {
+          code: file._getDouble(offset),
+          length: file._getDouble(offset + 4),
+          desc: file._getAsciiText(offset + 8, 32),
+          name: file._getAsciiText(offset + 40, 12),
+          width: file._getDouble(offset + 52),
+          height: file._getDouble(offset + 56),
+          pointCount: file._getDouble(offset + 60),
+          pointsOffset: offset + 64
+        };
+
+        entry.dataOffset = offset + 64 + (4 * entry.pointCount);
+        entry.size = entry.dataOffset + (entry.width * entry.height) - offset;
+        file._maps.push(entry);
+        file._confirmMap(file._length);
+        file._length++;
+
+        setTimeout(() => readMap(offset + entry.size));
+      }
+    });
+  }
+
+  _getDouble(offset): number {
+    return this._bytes[offset] |
+           this._bytes[offset + 1] << 8 |
+           this._bytes[offset + 2] << 16 |
+           this._bytes[offset + 3] << 24;
+  }
+
+  _getAsciiText(offset, max): string {
+    const chars = [];
+    for (let i = offset; max > 0 && this._bytes[i] !== 0; i++, max--) {
+      chars.push(String.fromCharCode(this._bytes[i]));
+    }
+    return chars.join('');
+  }
+
+  _confirmMap(index) {
+    if (Array.isArray(this._mapRequests[index])) {
+      this._mapRequests[index].forEach(([resolve]) => resolve(this._maps[index]));
+    }
+  }
+
+  _confirmAllMaps() {
+    for (let index = this._length; index < this._mapRequests.length; index++) {
+      this._mapRequests[index]
+      .forEach(([_, reject]) => reject(`There is no map with index ${index}`)); 
+    }
   }
 
 };
 
 export default {
   read(buffer: ArrayBuffer): Promise<FPGFile> {
-    return Promise.resolve(new FPGFile(buffer));
+    return this._assertFpg(buffer)
+    .then(bytes => new FPGFile(bytes));
+  },
+
+  _assertFpg(buffer: Uint8Array): Promise<Uint8Array> {
+    const bytes = new Uint8Array(buffer);
+    return new Promise((resolve, reject) => {
+      ['f', 'p', 'g', 0x1A, 0x0D, 0x0A, 0x00].forEach((code, index) => {
+        const char = typeof code === 'string' ? code.charCodeAt(0) : code;
+        if (bytes[index] !== char) {
+          reject('The file is not a FPG file');
+        }
+      });
+      resolve(bytes);
+    });
   }
 };
--- tests/fpg/fpg.test.ts
+++ tests/fpg/fpg.test.ts
@@ -3,14 +3,65 @@ import { assert } from 'chai';
 
 suite("FPG file reader", function () {
   let fileBuffer : ArrayBuffer;
+  
+  const nonFPG = new Uint8Array(1024).buffer;
 
-  setup(() => {
+  suiteSetup(() => {
     return fetch('/base/assets/TUTOR0.FPG')
     .then(response => response.arrayBuffer())
     .then(buffer => fileBuffer = buffer);
   });
 
-  test("Read a FPG file", () => {
+  test('Reads a FPG file', () => {
     return fpg.read(fileBuffer);
+  });
+
+  test('Rejects non FPG files', () => {
+    return fpg.read(nonFPG)
+    .then(
+      () => Promise.reject('Should throw'),
+      () => Promise.resolve()
+     );
+  });
+
+  test('Gets version', () => {
+    return fpg.read(fileBuffer)
+    .then(file => assert.equal(file.version, 0));
+  });
+
+  test('Reads the length asynchronously', () => {
+    return fpg.read(fileBuffer)
+    .then(file => file.length)
+    .then(length => assert.equal(length, 10));
+  });
+
+  test('Fails if reading a negative map', () => {
+    return fpg.read(fileBuffer)
+    .then(file => file.map(-10))
+    .then(
+      () => Promise.reject('Should throw'),
+      () => Promise.resolve()
+    );
+  });
+
+  test('Fails if trying to read an unavailable map', () => {
+    return fpg.read(fileBuffer)
+    .then(file => file.map(10))
+    .then(
+      () => Promise.reject('Should throw'),
+      () => Promise.resolve()
+    );
+  });
+
+  test('Reads the meta data of a map asynchronously', () => {
+    return fpg.read(fileBuffer)
+    .then(file => file.map(1))
+    .then(map => {
+      assert.equal(map.code, 2);
+      assert.equal(map.name, 'Mapa 4');
+      assert.equal(map.desc, 'Fondo');
+      assert.equal(map.width, 320);
+      assert.equal(map.height, 200);
+    });
   });
 });
